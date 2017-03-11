fbd860c87486ed5ba0f0e2695eb41ebc17aa27ed
eliot carlsen <eliot.carlsen@gmail.com>
2017-03-11 15:44:06 -0800

    string manipulation

---
+++ string_results.php
@@ -0,0 +1,26 @@
+<?php
+    $sentence1 = $_GET["string"];
+    $upper_sentence1 = strtoupper($sentence1);
+    $sentence2 = $_GET["string2"];
+    $word_number = str_word_count($sentence2);
+    $sentence3 = $_GET["string3"];
+    $shuffle_string = str_shuffle($sentence3);
+    $sentence4 = $_GET["string4"];
+    $letter_finder = stripos($sentence4, "man");
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php echo $upper_sentence1; ?>
+    <?php echo $word_number; ?>
+    <?php echo $shuffle_string; ?>
+    <?php echo $letter_finder; ?>
+  </body>
+</html>
